81f9836203d572a1245f472ff229cd8ec3130fb0
Caly <caly@inventa.com.au>
2018-06-20 15:07:06 +1000

    Fix up xcode react imports and add react header search path

--- ios/RTCSystemSetting.h
+++ ios/RTCSystemSetting.h
@@ -7,8 +7,8 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <React/RCTBridgeModule.h>
-#import <React/RCTEventEmitter.h>
+#import "RCTBridgeModule.h"
+#import "RCTEventEmitter.h"
 #import <CoreBluetooth/CoreBluetooth.h>
 
 @interface RCTSystemSetting : RCTEventEmitter <RCTBridgeModule, CBCentralManagerDelegate>
